8e3b33c36e877b4c4ece1f268e5ef893b09f3454
Nick Reed <nicolas.reed@hp.com>
2024-01-31 16:29:37 -0700

    formatting

--- Scripts/Utilities.js
+++ Scripts/Utilities.js
@@ -1,96 +1,96 @@
 function refreshPage() {
     location.reload();
-  }
+}
 
-  function strikeOutMonsterAction() {
+function strikeOutMonsterAction() {
     if (!gameCanStart) {
-      showErrorMessage("Please Start the Game First");
-      return;
+        showErrorMessage("Please Start the Game First");
+        return;
     }
     const diceLog = document.getElementById("diceLog");
     const logEntries = diceLog.getElementsByClassName("logEntry");
-  
+
     // Check if there are log entries
     if (logEntries.length > 0) {
-      // Strike out the most recent log entry
-      const mostRecentLog = logEntries[logEntries.length - 1];
-      if (!mostRecentLog.innerText.includes("ROUND") && !mostRecentLog.innerText.includes("HP")) {
-        mostRecentLog.style.textDecoration = "line-through";
-      }
+        // Strike out the most recent log entry
+        const mostRecentLog = logEntries[logEntries.length - 1];
+        if (!mostRecentLog.innerText.includes("ROUND") && !mostRecentLog.innerText.includes("HP")) {
+            mostRecentLog.style.textDecoration = "line-through";
+        }
     }
-  }
+}
 
-  function removeImage(img) {
+function removeImage(img) {
     const container = img.parentNode;
     container.removeChild(img);
-  }
+}
 
-  function showErrorMessage(message) {
+function showErrorMessage(message) {
     // Create a new error message element
     const errorMessageElement = document.createElement("div");
     errorMessageElement.className = "errorMessage";
     errorMessageElement.textContent = message;
-  
+
     // Append the error message element to the body
     document.body.appendChild(errorMessageElement);
-  
+
     // Display the error message
     errorMessageElement.style.display = "block";
-  
+
     // Hide the error message after 5 seconds
     setTimeout(function () {
-      errorMessageElement.style.display = "none";
-      // Remove the error message element from the DOM after fading out
-      errorMessageElement.remove();
+        errorMessageElement.style.display = "none";
+        // Remove the error message element from the DOM after fading out
+        errorMessageElement.remove();
     }, 5000);
-  }
+}
 
-  function addLog(logText) {
+function addLog(logText) {
     const logEntry = document.createElement("div");
     logEntry.className = "logEntry";
     logEntry.textContent = logText;
-  
+
     // Get the dice log container
     const diceLog = document.getElementById("diceLog");
-  
+
     // Append the new log entry to the top of the log
     diceLog.insertAdjacentElement("beforeend", logEntry);
     diceLog.scrollTop = diceLog.scrollHeight;
     diceLog.style.display = "block";
-  }
+}
 
-  function readActionJsonFiles() {
+function readActionJsonFiles() {
     const filePath = 'Actions/EasyActions.json';
     const filePathM = 'Actions/MediumActions.json';
     const filePathH = 'Actions/HardActions.json';
     const options = {
-      method: 'GET',
-      mode: 'no-cors'
+        method: 'GET',
+        mode: 'no-cors'
     };
-  
+
     // Fetch the JSON file
     fetch(filePath, options)
-      .then(
-        response => response.json())
-      .then(jsonData => {
-        console.log('JSON data:', jsonData);
-        easyActionsJson = jsonData;
-      })
-      .catch(error => console.error('Error fetching JSON:', error));
-  
+        .then(
+            response => response.json())
+        .then(jsonData => {
+            console.log('JSON data:', jsonData);
+            easyActionsJson = jsonData;
+        })
+        .catch(error => console.error('Error fetching JSON:', error));
+
     fetch(filePathM, options)
-      .then(response => response.json())
-      .then(jsonData => {
-        console.log('JSON data:', jsonData);
-        mediumActionsJson = jsonData;
-      })
-      .catch(error => console.error('Error fetching JSON:', error));
-  
+        .then(response => response.json())
+        .then(jsonData => {
+            console.log('JSON data:', jsonData);
+            mediumActionsJson = jsonData;
+        })
+        .catch(error => console.error('Error fetching JSON:', error));
+
     fetch(filePathH, options)
-      .then(response => response.json())
-      .then(jsonData => {
-        console.log('JSON data:', jsonData);
-        hardActionsJson = jsonData;
-      })
-      .catch(error => console.error('Error fetching JSON:', error));
-  }
+        .then(response => response.json())
+        .then(jsonData => {
+            console.log('JSON data:', jsonData);
+            hardActionsJson = jsonData;
+        })
+        .catch(error => console.error('Error fetching JSON:', error));
+}
