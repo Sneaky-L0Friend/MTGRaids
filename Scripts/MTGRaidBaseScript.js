8a43d5e4d4b9214e572e4a635ae4aca3b686d1cc
Nick Reed <nicolas.reed@hp.com>
2024-01-31 14:27:26 -0700

    more move

--- Scripts/MTGRaidBaseScript.js
+++ Scripts/MTGRaidBaseScript.js
@@ -120,24 +120,6 @@ function takeMonsterAction() {
 
 }
 
-function strikeOutMonsterAction() {
-  if (!gameCanStart) {
-    showErrorMessage("Please Start the Game First");
-    return;
-  }
-  const diceLog = document.getElementById("diceLog");
-  const logEntries = diceLog.getElementsByClassName("logEntry");
-
-  // Check if there are log entries
-  if (logEntries.length > 0) {
-    // Strike out the most recent log entry
-    const mostRecentLog = logEntries[logEntries.length - 1];
-    if (!mostRecentLog.innerText.includes("ROUND") && !mostRecentLog.innerText.includes("HP")) {
-      mostRecentLog.style.textDecoration = "line-through";
-    }
-  }
-}
-
 function readActionJsonFiles() {
   const filePath = 'Actions/EasyActions.json';
   const filePathM = 'Actions/MediumActions.json';
@@ -327,11 +309,6 @@ function addMinions(numberOfImages, imageNumber) {
     imageContainer.appendChild(imageText);
     container.appendChild(imageContainer);
   }
-}
-
-function removeImage(img) {
-  const container = img.parentNode;
-  container.removeChild(img);
 }
 
 function setDifficultyAtStart(difficulty) {
--- Scripts/Utilities.js
+++ Scripts/Utilities.js
@@ -1,3 +1,26 @@
 function refreshPage() {
     location.reload();
   }
+
+  function strikeOutMonsterAction() {
+    if (!gameCanStart) {
+      showErrorMessage("Please Start the Game First");
+      return;
+    }
+    const diceLog = document.getElementById("diceLog");
+    const logEntries = diceLog.getElementsByClassName("logEntry");
+  
+    // Check if there are log entries
+    if (logEntries.length > 0) {
+      // Strike out the most recent log entry
+      const mostRecentLog = logEntries[logEntries.length - 1];
+      if (!mostRecentLog.innerText.includes("ROUND") && !mostRecentLog.innerText.includes("HP")) {
+        mostRecentLog.style.textDecoration = "line-through";
+      }
+    }
+  }
+
+  function removeImage(img) {
+    const container = img.parentNode;
+    container.removeChild(img);
+  }
